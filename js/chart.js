5a0566f9352718f035cbf781521f0de7a85ebb4a
Suganth <sugupr1234@gmail.com>
2022-11-04 16:10:01 +0530

    temp commit

---
+++ js/chart.js
@@ -0,0 +1,29 @@
+const labels = [
+    'Mentor1',
+    'Mentor2',
+    'Mentor3',
+    'Mentor4',
+    'Mentor5',
+    'Mentor6',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,13 @@ $('.profile-img').click(function() {
     $('.body_cont').html('');
     $('.body_cont').load('profile.html');
 })
+
+$('.mentors').click(function() {
+    $('.body_cont').html('');
+    $('.body_cont').load('mentors.html');
+})
+
+$('.chart').click(function() {
+    $('.body_cont').html('');
+    $('.body_cont').load('chart.html');
+})
